939427bec989e26754195a9f5655e073fafeb91a
jonasrost <73895167+jonasrost@users.noreply.github.com>
2023-10-12 16:00:12 +0100

    Fix so that data is imported correctly

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // Global data variable
 // Define a global variable to store the loaded CSV data
-let csvData;
+var csvData;
 
 // Define margin and dimensions for the charts
 const margin = {
@@ -21,17 +21,43 @@ function startDashboard() {
     return d3.csv(file);
   }
 
-  // Function to load CSV data using D3.js
-  d3.csv('GamesData.csv').then(function(data) {
-  // Data loading is asynchronous, so handle the data inside this callback function
-    csvData = data;
-    console.log('CSV data loaded:', csvData);
-  // Call any functions or perform operations with the data here
-  }).catch(function(error) {
-    console.error('Error:', error);
-  });
+  function importFiles(file1) {
+    return Promise.all([loadCSV(file1)]);
+  }
+  
+  importFiles('GamesData.csv').then(function (results) {
+    csvData = results[0];
+
+    
+    // Convert incomeperperson and alcconsumption data to numbers
+    csvData.forEach(function (d) {
+      d.tm = +d.tm 
+      d.opp_score = +d.opp_score
+      d.fg = +d.fg
+      d.fga = +d.fga
+      d.fg_percentage = + d.fg_percentage
+      d.three_points = +d.three_points
+      d.three_point_attempts = +d.three_point_attempts
+      d.three_point_percentage = +d.three_point_percentage
+      d.ft = +d.ft
+      d.fta = +d.fta
+      d.free_throw_percentage = +d.free_throw_percentage
+      d.orb = +d.orb
+      d.trb = +d.trb
+      d.ast = +d.ast
+      d.stl = +d.stl
+      d.blk = +d.blk
+      d.tov = +d.tov
+      d.pf = +d.pf
+      d.drb = +d.drb
+      d.two_point_percentage = +d.two_point_percentage
+    });
+    
+
 
     // Call functions to create the plots
     createParallelCoordinates(); //Define width inside this function
     createDensityPlot(); //Define width inside this function
-};
+  })
+
+  };
