cc4808c897df9679c24e5dabf1f3059a19804698
Ivy.Tumoine <ivy.tumoine@insa-rennes.fr>
2023-10-12 18:28:09 +0200

    simplest wins bar chart possible

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ const margin = {
 };
 
 
-//const width = 500 - margin.left - margin.right;
+const width = 500 - margin.left - margin.right;
 const height = 400 - margin.top - margin.bottom;
 
 // Function to start the dashboard
@@ -56,8 +56,90 @@ function startDashboard() {
 
 
     // Call functions to create the plots
-    createParallelCoordinates(); //Define width inside this function
-    createDensityPlot(); //Define width inside this function
+    // createParallelCoordinates(); //Define width inside this function
+    // createDensityPlot(); //Define width inside this function
+    createBarCharts();
   })
 
   };
+
+function createBarCharts(){
+  // first make a count for each opponent team of wins and loses.
+  opptable = winsandlosses(csvData)
+  const svgW = d3
+    .select("#barCharts")
+    .append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+    .attr("transform", `translate(${margin.left},${margin.top})`);
+
+  // Define scales
+  const yScale = d3.scaleBand()
+    .domain(opptable.map(d => d.name))
+    .range([height - margin.top - margin.bottom, 0])
+    .padding(0.3);
+
+  const xScaleW = d3.scaleLinear()
+    .domain([0, d3.max(opptable, d => d.wins)])
+    .nice()
+    .range([0, width - margin.left - margin.right])
+
+  // Create and append the bars
+  svgW.selectAll(".bar")
+    .data(opptable)
+    .enter()
+    .append("rect")
+    .attr("class", "bar")
+    .attr("x", xScaleW(0))
+    .attr("y", d => yScale(d.name))
+    .attr("width",d => xScaleW(d.wins))
+    .attr("height", yScale.bandwidth());
+
+  // Add x-axis
+  svgW.append("g")
+    .attr("class", "x-axis")
+    .attr("transform", `translate(0, ${height - margin.top - margin.bottom})`)
+    .call(d3.axisBottom(xScaleW));
+
+  // Add y-axis
+  svgW.append("g")
+    .attr("class", "y-axis")
+    .call(d3.axisLeft(yScale));
+}
+
+function winsandlosses(csvData){
+  oppDict = {}
+  csvData.forEach(element => {
+    if(!(element.opp in oppDict)){
+      oppDict[element.opp] = {"wins" : 0, "losses" : 0}
+    }
+    if(element["w/l"] == "W"){
+      oppDict[element.opp]["wins"] ++
+    }else{
+      oppDict[element.opp]["losses"] ++
+    }
+  });
+
+  for (const key in oppDict){
+    if(oppDict[key].wins == 0 && oppDict[key].losses == 0){
+      console.log(key)
+    }
+  }
+
+  console.log(oppDict["NOK"])
+
+  // convert to a table
+  const tableOpp = [];
+  for (const key in oppDict) {
+    if (oppDict.hasOwnProperty(key)) {
+      const item = {
+        "name": key,
+        "wins": oppDict[key].wins,
+        "losses" : oppDict[key].losses
+      };
+      tableOpp.push(item);
+    }
+  }
+  return tableOpp;
+}
