381e84417ed3019d80b9e53b78280e93d6048d0e
jonasrost <73895167+jonasrost@users.noreply.github.com>
2023-10-12 16:03:11 +0100

    First day of work

--- script.js
+++ script.js
@@ -60,4 +60,196 @@ function startDashboard() {
     createDensityPlot(); //Define width inside this function
   })
 
-  };
+  }
+
+  
+function createParallelCoordinates() {
+
+  const width = 600; // - margin.left - margin.right;
+  const svg = d3
+    .select("#parallelCoordinates")
+    .append("svg")
+      .attr("width", width + margin.left + margin.right)
+      .attr("height", height + margin.bottom + margin.top)
+    .append("g")
+      .attr("transform",
+            `translate(${margin.left},${margin.top})`);
+
+  // Color scale: different colors for each season
+  var colorScale = d3.scaleOrdinal()
+    .domain(["2022-23", "2021-22", "2020-21", "2019-20", "2018-19", "2017-18", "2016-17", "2015-16", "2014-15", "2013-14", "2012-13", "2011-12", "2010-11", "2009-10", "2008-09", "2007-08", "2006-07", "2005-06", "2004-05", "2003-04", "2002-03"])
+    .range(getHueScaleColors(20, 50, 300));
+
+
+  // Choose dimensions to include in the plot
+  dimensions = ["fg_percentage", "free_throw_percentage", "ast", "orb", "drb", "stl", "blk"];
+
+  
+  // Create an object to store your scales
+  const yScale = {};
+
+  // For each dimension, build a linear scale
+  dimensions.forEach(function(dimension) {
+    // Get the min and max values for the dimension
+    const min = d3.min(csvData, d => +d[dimension]);
+    const max = d3.max(csvData, d => +d[dimension]);
+
+    // Create a linear scale for the dimension
+    const scale = d3.scaleLinear()
+      .domain([min, max])
+      .range([height, 0]); 
+
+    // Store the scale in the yScale
+    yScale[dimension] = scale;
+  });
+
+  xScale = d3.scalePoint()
+    .range([0, width])
+    .domain(dimensions);
+
+  // Highlight the season that is hovered
+  const highlight = function(event, d){
+
+    selected_season = d.season
+
+    showTooltip(event, selected_season);
+
+    // first every group turns grey
+    d3.selectAll(".line")
+      .transition().duration(10)
+      .style("stroke", "lightgrey")
+      .style("opacity", "0.2")
+    // Second the hovered season takes its color
+    d3.selectAll(".season-" + selected_season)
+      .transition().duration(200)
+      .style("stroke", colorScale(selected_season))
+      .style("opacity", "1")
+  }
+
+  // Unhighlight
+  const doNotHighlight = function(event, d){
+
+    hideTooltip();
+
+    d3.selectAll(".line")
+      .transition().duration(10)
+      .style("stroke", function(d){ return( colorScale(d.season))} )
+      .style("opacity", "1")
+  }
+
+  // Function to show tooltip
+function showTooltip(event, season) {
+  // Create or select the tooltip element
+  let tooltip = d3.select("#tooltip");
+
+  // Show the tooltip
+  tooltip.transition().duration(10).style("opacity", 0.9);
+
+  // Position the tooltip at the cursor
+  tooltip.style("left", (event.pageX + 10) + "px")
+    .style("top", (event.pageY - 20) + "px");
+
+  // Set the tooltip text
+  tooltip.text(season);
+}
+
+// Function to hide the tooltip
+function hideTooltip() {
+  // Hide the tooltip
+  d3.select("#tooltip").transition().duration(10).style("opacity", 0);
+}
+
+  // The path function take a row of the csv as input, and return x and y coordinates of the line to draw for this raw.
+  function path(d) {
+    return d3.line()(dimensions.map(function(p) { return [xScale(p), yScale[p](d[p])]; }));
+  }
+
+  // Draw the lines
+  svg.selectAll("path")
+    .data(csvData)
+    .join("path")
+      .attr("class", function (d) { return "line season-" + d.season } ) // 2 class for each line: 'line' and the group name
+      .attr("d",  (d) => path(d))
+      .style("fill", "none" )
+      .style("stroke", function(d){ return( colorScale(d.season))} )
+      .style("opacity", 0.5)
+      .on("mouseover", highlight)
+      .on("mouseleave", doNotHighlight )
+
+
+   // Draw the axis:
+   svg.selectAll("myAxis")
+   // For each dimension of the dataset I add a 'g' element:
+   .data(dimensions).enter()
+   .append("g")
+   .attr("class", "axis")
+   // I translate this element to its right position on the x axis
+   .attr("transform", function(d) { return "translate(" + xScale(d) + ")"; })
+   // And I build the axis with the call function
+   .each(function(d) { d3.select(this).call(d3.axisLeft().ticks(5).scale(yScale[d])); })
+   // Add axis title
+   .append("text")
+     .style("text-anchor", "middle")
+     .attr("y", -9)
+     .text(function(d) { return d; })
+     .style("fill", "black");
+}
+
+
+
+function getHueScaleColors(count, startHue, endHue) {
+  const colors = [];
+
+  for (let i = 0; i < count; i++) {
+    // Generate hue within the specified range
+    const hue = startHue + ((endHue - startHue) * (i / count));
+
+    // Set constant saturation and value
+    const saturation = 90;
+    const value = 90;
+
+    // Convert HSV to RGB
+    const chroma = (value / 100) * (saturation / 100);
+    const x = chroma * (1 - Math.abs(((hue / 60) % 2) - 1));
+    const m = value / 100 - chroma;
+
+    let r, g, b;
+    if (hue >= 0 && hue < 60) {
+      r = chroma;
+      g = x;
+      b = 0;
+    } else if (hue >= 60 && hue < 120) {
+      r = x;
+      g = chroma;
+      b = 0;
+    } else if (hue >= 120 && hue < 180) {
+      r = 0;
+      g = chroma;
+      b = x;
+    } else if (hue >= 180 && hue < 240) {
+      r = 0;
+      g = x;
+      b = chroma;
+    } else if (hue >= 240 && hue < 300) {
+      r = x;
+      g = 0;
+      b = chroma;
+    } else {
+      r = chroma;
+      g = 0;
+      b = x;
+    }
+
+    // Convert RGB values to integers
+    const red = Math.floor((r + m) * 255);
+    const green = Math.floor((g + m) * 255);
+    const blue = Math.floor((b + m) * 255);
+
+    // Create an RGB color string
+    const color = `rgb(${red},${green},${blue}`;
+
+    colors.push(color);
+  }
+
+  return colors;
+}
